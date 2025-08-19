bee0ed3203abc7e13f366664bb67924b1c39da13
saha-7op <saha7pritam@gmail.com>
2025-08-19 22:34:41 +0530

    Button added with dynamic props name

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({name}) => {
   return (
     <div>
-      <button className='px-4 py-2 m-2 mt-3 rounded-xl bg-gray-200'>All</button>
+      <button className='px-4 py-2 m-1 mt-3 rounded-xl bg-gray-200'>{name}</button>
     </div>
   )
 }
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -4,21 +4,21 @@ import Button from './Button'
 const ButtonList = () => {
   return (
     <div className='flex'>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
+      <Button name="All"/>
+      <Button name="Songs"/>
+      <Button name="Live"/>
+      <Button name="BGMI"/>
+      <Button name="Football"/>
+      <Button name="La Liga"/>
+      <Button name="Premiere League"/>
+      <Button name="Machine learning"/>
+      <Button name="AI"/>
+      <Button name="Gen AI"/>
+      <Button name="Python"/>
+      <Button name="Movies"/>
+      <Button name="Travel"/>
+      <Button name="News"/>
+      <Button name="Fashion"/>
     </div>
   )
 }
