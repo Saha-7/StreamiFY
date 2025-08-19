bf6559c6dd223189ad5088b884b998bf7e029368
saha-7op <saha7pritam@gmail.com>
2025-08-19 23:44:18 +0530

    Button list map

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import Button from './Button'
 
+const list = [
+  "All", "Songs", "Live", "BGMI", "Football", 
+  "La Liga", "Premiere League", "Machine learning", 
+  "AI", "Gen AI", "Python", "Movies", 
+  "Travel", "News", "Fashion"
+]
+
 const ButtonList = () => {
   return (
     <div className='flex'>
-      <Button name="All"/>
-      <Button name="Songs"/>
-      <Button name="Live"/>
-      <Button name="BGMI"/>
-      <Button name="Football"/>
-      <Button name="La Liga"/>
-      <Button name="Premiere League"/>
-      <Button name="Machine learning"/>
-      <Button name="AI"/>
-      <Button name="Gen AI"/>
-      <Button name="Python"/>
-      <Button name="Movies"/>
-      <Button name="Travel"/>
-      <Button name="News"/>
-      <Button name="Fashion"/>
+      {list.map((item, index)=>
+        <Button key={index} name={item} />
+       )}
     </div>
   )
 }
