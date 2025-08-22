e223374c7f191d327888c63ec6e8b1bab9d8366a
saha-7op <saha7pritam@gmail.com>
2025-08-22 15:08:13 +0530

    Youtube Api call added with .env file

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { YOUTUBE_VIDEO_API } from '../utils/constant'
 
 const VideoContainer = () => {
+  useEffect(()=>{
+    getVideos()
+  },[])
+
+  const getVideos = async()=>{
+    const response = await fetch(YOUTUBE_VIDEO_API)
+    const data = await response.json()
+    console.log(data)
+  }
+
+
   return (
     <div>
       VideoContainer
