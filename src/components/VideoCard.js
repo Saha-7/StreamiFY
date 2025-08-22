97bace0094e25ebbef61423bd2fd218df0572868
saha-7op <saha7pritam@gmail.com>
2025-08-22 17:14:22 +0530

    video test

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 const VideoCard = ({info}) => {
+
+  const {snippet, statistics} = info
+  const {channelTitle, thumbnails, title}=snippet
+  const {likeCount, viewCount} = statistics
   console.log(info)
+
   return (
-    <div>
-      VideoCard
+    <div className='p-2 m-2 w-60 shadow-lg'>
+      <img className='rounded-lg' src={thumbnails.standard.url} alt='thumbnail' />
+      <ul>
+        <li className='py-2 font-bold'>{title}</li>
+        <li className='font-semibold'>{channelTitle}</li>
+        <li>{viewCount} views</li>
+      </ul>
     </div>
   )
 }
