f3a7698df550149a5694a54fa34efc125e882962
saha-7op <saha7pritam@gmail.com>
2025-08-24 16:23:08 +0530

    watch page & query params setup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   const isMenuOpen = useSelector((store)=>store.app.isMenuOpen)
@@ -13,7 +14,11 @@ const Sidebar = () => {
   return (
     <div className='p-5 space-y-1 shadow-lg w-52'>
       <ul>
-        <li>Home</li>
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
         <li>Shorts</li>
         <li>Videos</li>
         <li>Live</li>
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
+  const [searchParams] = useSearchParams();
+  const videoId = searchParams.get("v");
+  
+  console.log("Video ID:", videoId); // a0Bg0srLKGU
+  
   return (
     <div>
-      
+      <h1>Watching video: {videoId}</h1>
+      {/* Your video player here */}
     </div>
-  )
-}
+  );
+};
 
-export default WatchPage
+
+export default WatchPage;
